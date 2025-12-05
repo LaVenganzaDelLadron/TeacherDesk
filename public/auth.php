64ad19ced1db80ec2872794739b517f19e64b2bd
DarkGlitch <darkglitch5417@gmail.com>
2025-12-06 03:39:02 +0800

    first commit

---
+++ public/auth.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    
+    <body>
+
+        <form action="index.php" method="post">
+            <div>
+                <h1>Signup</h1>
+                <div>
+                    <input type="text" name="rUsername" class="rUsername" placeholder="Username" required>
+                </div>
+                <div>
+                    <input type="email" name="rEmail" class="rEmail" placeholder="Email" required>
+                </div>
+                <div>
+                    <input type="password" name="rPassword" class="rPassword" placeholder="Password" required>
+                </div>
+                <div>
+                    <input type="submit" name="rSubmit" class="rSubmit">
+                </div>
+            </div>
+        </form>
+    
+    </body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+    <head>
+        <title>TeacherDesk</title>
+    </head>
+    <body>
+
+        <a href="auth.php"><button>Login</button></a>    
+
+
+    </body>
+</html>
